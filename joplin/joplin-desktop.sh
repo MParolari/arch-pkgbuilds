6d67137baa45f27dbeebea4cada49ceb2ee0b738
MParolari <mparolari.dev@gmail.com>
2020-12-26 19:24:35 +0100

    Desktop version (packaging)

--- joplin/joplin-desktop.sh
+++ joplin/joplin-desktop.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-readonly joplin_dir="/usr/share/joplin-desktop/"
-
-if [[ ! -d $joplin_dir ]]; then
-  echo "Cannot find /usr/share/joplin-desktop/"
-  exit 1
-fi
-
-cd $joplin_dir
-
-./@joplinapp-desktop "$@"
-
+exec electron /usr/lib/joplin-desktop.asar "$@"
